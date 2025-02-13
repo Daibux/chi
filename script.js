e30a960a978c4cfbcf92c2b2709da443003b8dcd
Daibux <adriankolokoy18@gmail.com>
2025-02-13 21:54:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "See u nigger";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
